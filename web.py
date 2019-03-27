d4f3e1806730d297b2db0246ebccaa5efd1602f9
MAXS30 <bgsod14159@gmail.com>
2019-03-27 19:56:48 +0900

    test

--- web.py
+++ web.py
@@ -1,10 +1,15 @@
-import flask
+from flask import Flask,render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World"
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
